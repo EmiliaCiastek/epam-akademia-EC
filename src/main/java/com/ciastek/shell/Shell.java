15fdfee8717b3ba7980d8ba8518fbc9eae5637b5
emcia <emciastek@gmail.com>
2017-10-17 11:13:52 +0200

    Commands dir and exit added

--- src/main/java/com/ciastek/shell/Shell.java
+++ src/main/java/com/ciastek/shell/Shell.java
@@ -1,9 +1,47 @@
 package com.ciastek.shell;
 
+import java.io.File;
+import java.util.Arrays;
+
 public class Shell {
     private String commandResult = "";
+    private File currentDirectory;
+    private boolean running = true;
+
+    public Shell(){
+        currentDirectory = new File(System.getProperty("user.dir"));
+    }
 
     public String receiveCommand(String command){
+
+        switch (command){
+            case "dir":
+                commandResult += "Content of " + currentDirectory.getAbsolutePath();
+                File files[] =  currentDirectory.listFiles();
+                Arrays.sort(files);
+
+                for (File file:files) {
+                    if (file.isFile()){
+                        commandResult += "\nFILE \t" + file.getName();
+                    } else {
+                        commandResult += "\nDIR \t" + file.getName();
+                    }
+                }
+                break;
+            case "exit":
+                running = false;
+                commandResult = "";
+                break;
+            default:
+                commandResult = "";
+
+        }
+
         return commandResult;
     }
+
+    public boolean isRunning(){
+        return running;
+    }
+
 }
--- src/main/java/com/ciastek/shell/ShellApplication.java
+++ src/main/java/com/ciastek/shell/ShellApplication.java
@@ -1,10 +1,22 @@
 package com.ciastek.shell;
 
+import java.util.Scanner;
+
 public class ShellApplication {
     private static Shell shell;
 
     public static void main(String[] Args){
+        Scanner input = new Scanner(System.in);
         shell = new Shell();
+
+
+        while (shell.isRunning()){
+            System.out.print("$>");
+            String command = input.nextLine();
+            System.out.println(shell.receiveCommand(command));
+        }
+
+
 
 
     }
